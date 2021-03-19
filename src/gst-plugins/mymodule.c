6f527837a707e9287bfbb6d1d49f2eebb9706b94
Kozo Komiya <kozo.komiya@gmail.com>
2021-03-19 00:39:35 +0000

    Initial commit

---
+++ src/gst-plugins/mymodule.c
@@ -0,0 +1,18 @@
+#include <config.h>
+#include <gst/gst.h>
+
+#include "gstmymodule.h"
+
+static gboolean
+init (GstPlugin *plugin)
+{
+  if (!gst_my_module_plugin_init (plugin))
+    return FALSE;
+
+  return TRUE;
+}
+
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR,
+    mymodule,
+    "Filter documentation",
+    init, VERSION, GST_LICENSE_UNKNOWN, "PACKAGE_NAME", "origin")
